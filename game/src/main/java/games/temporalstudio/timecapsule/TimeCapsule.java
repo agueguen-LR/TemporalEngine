3090d45bb01079d734594888c2eb76aa7776d9b5
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-26 13:43:06 +0200

    Made a singleton

--- game/src/main/java/games/temporalstudio/timecapsule/TimeCapsule.java
+++ game/src/main/java/games/temporalstudio/timecapsule/TimeCapsule.java
@@ -3,5 +3,16 @@ package games.temporalstudio.timecapsule;
 import games.temporalstudio.temporalengine.Game;
 
 public class TimeCapsule extends Game{
-	
+	private static TimeCapsule instance;
+
+	private TimeCapsule() {
+		TimeCapsule.instance = this;
+	}
+
+	public static TimeCapsule get(){
+		if (TimeCapsule.instance == null) {
+			TimeCapsule.instance = new TimeCapsule();
+		}
+		return TimeCapsule.instance;
+	}
 }
