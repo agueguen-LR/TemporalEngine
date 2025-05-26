25be36c359da724e5613db38c4f91b2e79617385
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-26 14:38:40 +0200

    Better component handling

--- game/src/main/java/games/temporalstudio/timecapsule/GameObject.java
+++ game/src/main/java/games/temporalstudio/timecapsule/GameObject.java
@@ -1,17 +1,28 @@
 package games.temporalstudio.timecapsule;
 
+import java.util.HashMap;
+
 public class GameObject implements LifeCycle {
 	private String name;
 	private Scene currentScene;
 
-	private Component[] components;
+	private HashMap<Class<? extends Component>, Component> components;
 
 	public GameObject(String name, Scene currentScene) {
 		this.name = name;
 		this.currentScene = currentScene;
-		this.components = new Component[0]; // Initialize with an empty array
+		this.components = new HashMap<>(); // Initialize with an empty array
 	}
 
+	public void addComponent(Component component) {
+		if (component != null) {
+			components.put(component.getClass(), component);
+		}
+	}
+
+	public <T extends Component> T getComponent(Class<T> componentClass) {
+		return componentClass.cast(components.get(componentClass));
+	}
 
 	@Override
 	public void init() {
