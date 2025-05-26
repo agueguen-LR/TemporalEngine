216dd9f77a6f0fd742f3a8ff68c508c6715eedd6
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-26 14:39:14 +0200

    Added Window display with update and deltaTime

--- engine/src/main/java/games/temporalstudio/temporalengine/Game.java
+++ engine/src/main/java/games/temporalstudio/temporalengine/Game.java
@@ -5,8 +5,16 @@ public abstract class Game extends App{
 	private Renderer renderer;
 	private PhysicsEngine physicsEngine;
 
+	public Game(String title) {
+		this.window = new Window(this::update, title);
+	}
+
 	@Override
 	public void run(String[] args) {
-		System.out.println(args[0]);
+		window.run();
+	}
+
+	public void update(float deltaTime) {
+		System.out.println(1/deltaTime + "FPS");
 	}
 }
--- game/src/main/java/games/temporalstudio/timecapsule/TimeCapsule.java
+++ game/src/main/java/games/temporalstudio/timecapsule/TimeCapsule.java
@@ -4,15 +4,18 @@ import games.temporalstudio.temporalengine.Game;
 
 public class TimeCapsule extends Game{
 	private static TimeCapsule instance;
+	private Scene scene;
 
-	private TimeCapsule() {
+	private TimeCapsule(String title) {
+		super(title);
 		TimeCapsule.instance = this;
 	}
 
 	public static TimeCapsule get(){
 		if (TimeCapsule.instance == null) {
-			TimeCapsule.instance = new TimeCapsule();
+			TimeCapsule.instance = new TimeCapsule("Time Capsule");
 		}
 		return TimeCapsule.instance;
 	}
+
 }
