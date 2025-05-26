9cf5d690227ddf929985965cdd085b7045e4ca9d
Xibitol <xibitol@pimous.dev>
2025-05-26 15:32:13 +0200

    Adapted Game class.

--- engine/src/main/java/games/temporalstudio/temporalengine/Game.java
+++ engine/src/main/java/games/temporalstudio/temporalengine/Game.java
@@ -1,20 +1,33 @@
 package games.temporalstudio.temporalengine;
 
+import java.io.Console;
+
 public abstract class Game extends App{
+
+	private static final String DEFAULT_WINDOW_TITLE = "Temporal Engine Game";
+
 	private Window window;
 	private Renderer renderer;
 	private PhysicsEngine physicsEngine;
 
-	public Game(String title) {
+	public Game(String title){
 		this.window = new Window(this::update, title);
 	}
-
-	@Override
-	public void run(String[] args) {
-		window.run();
+	public Game(){
+		this(DEFAULT_WINDOW_TITLE);
 	}
 
-	public void update(float deltaTime) {
+	// FUNCTIONS
+	@Override
+	public void run(String[] args){
+		window.run();
+	}
+	@Override
+	public void run(Console console, String[] args) {
+		throw new RuntimeException();
+	}
+
+	public void update(float deltaTime){
 		System.out.println(1/deltaTime + "FPS");
 	}
 }
