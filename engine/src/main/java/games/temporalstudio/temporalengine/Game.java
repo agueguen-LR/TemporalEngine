442576bbc701f903d7b9fb1701ba8fe687ff0422
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-26 13:44:47 +0200

    Added attributes

--- engine/src/main/java/games/temporalstudio/temporalengine/Game.java
+++ engine/src/main/java/games/temporalstudio/temporalengine/Game.java
@@ -1,6 +1,10 @@
 package games.temporalstudio.temporalengine;
 
 public abstract class Game extends App{
+	private Window window;
+	private Renderer renderer;
+	private PhysicsEngine physicsEngine;
+
 	@Override
 	public void run(String[] args) {
 		System.out.println(args[0]);
