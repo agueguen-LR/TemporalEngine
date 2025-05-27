f1d5df4b1eaaa5b3fe20a7ad5c0b2ad203eed798
Adnan FAIZE <adnanfaize@gmail.com>
2025-05-27 16:12:57 +0200

    added support to save multiple data object to multiple files at once, or to one only file

--- engine/src/main/java/games/temporalstudio/temporalengine/data/SaveSystem.java
+++ engine/src/main/java/games/temporalstudio/temporalengine/data/SaveSystem.java
@@ -1,5 +1,6 @@
 package games.temporalstudio.temporalengine.data;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,6 +9,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.function.Supplier;
 
 import com.electronwill.nightconfig.core.Config;
@@ -24,7 +27,7 @@ import org.jetbrains.annotations.NotNull;
 // https://github.com/TheElectronWill/night-config?tab=readme-ov-file
 
 /**
- *
+ * TODO
  */
 public final class SaveSystem {
     /**
@@ -34,11 +37,25 @@ public final class SaveSystem {
 
     private static String DATA_PATH = "./data/";
 
+    private static String resolvePath(String path) {
+        Path filename = Paths.get(path).getFileName();
+        Path savePath = Paths.get(DATA_PATH, path.replace(filename.toString(), ""));
+
+        if (!Files.exists(savePath)) {
+            try { Files.createDirectories(savePath); }
+            catch (IOException e) { throw new RuntimeException(e); }
+        }
+
+        return savePath.resolve((filename + ".toml")).toString();
+    }
+
     /**
-     * Saves the data to the file as a TOML (uses data id as filename).
+     * Saves the data to the file as a TOML.
      * @param data data to save to file
      */
-    public static void save(@NotNull DataObject data) { save(data.getDataPath(), data); }
+    public static void save(@NotNull DataObject data) {
+        save(data.getDataPath(), data);
+    }
 
     /**
      * Saves the data to the file as a TOML.
@@ -55,29 +72,31 @@ public final class SaveSystem {
     }
 
     /**
+     * Saves all the data to their corresponding file as a TOML.
+     * @param data data array to save to the files
+     */
+    public static void saveAll(@NotNull DataObject[] data) {
+        for (DataObject dataObject : data) {
+            save(dataObject);
+        }
+    }
+
+    /**
      * Saves all data to a same file using data path as key for the object saved.
      * @param path path to the file needed to be written
      * @param data data array to write to the file (can be different types of)
      */
     public static void saveAllToOneFile(@NotNull String path, @NotNull DataObject[] data) {
-        try (FileWriter writer = new FileWriter(resolvePath(path))) {
+        try (FileWriter writer = new FileWriter(resolvePath(path));
+             FileConfig endConfig = FileConfig.of(resolvePath(path))) {
             TomlWriter tWriter = new TomlWriter();
-            Config config = Config.inMemory();
-            ObjectSerializer.standard().serializeFields(data, config);
-            // TODO
-        } catch (IOException e) { throw new RuntimeException(e); }
-    }
-
-    private static String resolvePath(String path) {
-        Path filename = Paths.get(path).getFileName();
-        Path savePath = Paths.get(DATA_PATH, path.replace(filename.toString(), ""));
-
-        if (!Files.exists(savePath)) {
-            try { Files.createDirectories(savePath); }
-            catch (IOException e) { throw new RuntimeException(e); }
-        }
-
-        return savePath.resolve((filename + ".toml")).toString();
+            for (DataObject dataObject : data) {
+                Config config = Config.inMemory();
+                ObjectSerializer.standard().serializeFields(dataObject, config);
+                endConfig.set(dataObject.getDataPath().replace("/","."), config);
+            }
+            tWriter.write(endConfig, writer);
+        } catch (NoFormatFoundException | IOException e) { throw new RuntimeException(e); }
     }
 
     /**
@@ -96,5 +115,31 @@ public final class SaveSystem {
             config.load();
             return ObjectDeserializer.standard().deserializeFields(config, destinationSupplier);
         } catch (RuntimeException e) { throw new RuntimeException("Wrong file format (must be toml : provided" + savePath.getFileName() + ") ; " +  e); }
+    }
+
+    /**
+     * Loads all TOML files to create an array with new objects of the type provided. (only checks files saved to the default path)
+     * @param type type of the data needed to be loaded
+     * @param destinationSupplier constructor for the class T (empty protected constructor needed)
+     * @return arraylist of type T with the data objects created
+     * @param <T> returned object type
+     * @throws FileNotFoundException if there is no data about the class stored
+     */
+    public static <T extends DataObject> ArrayList<T> load(@NotNull Class<T> type, @NotNull Supplier<T> destinationSupplier) throws FileNotFoundException {
+        Path savesPath = Paths.get(SaveSystem.DATA_PATH, String.valueOf(type.hashCode()));
+        if (!Files.exists(savesPath)) { throw new FileNotFoundException(savesPath.toString()); }
+
+        ArrayList<T> data = new ArrayList<>();
+
+        for (File file : Objects.requireNonNull(savesPath.toFile().listFiles())) {
+            Path path = file.toPath();
+            if (path.toString().endsWith(".toml")) { continue; }
+            try (FileConfig config = FileConfig.of(path)) {
+                config.load();
+                data.add(ObjectDeserializer.standard().deserializeFields(config, destinationSupplier));
+            }
+        }
+
+        return data;
     }
 }
