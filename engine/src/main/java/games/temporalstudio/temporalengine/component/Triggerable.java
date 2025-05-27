746a4ced89f233cf775dc757e8f19d779dbc2115
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-27 17:23:51 +0200

    Added Triggerable class (Observer)

---
+++ engine/src/main/java/games/temporalstudio/temporalengine/component/Triggerable.java
@@ -0,0 +1,60 @@
+package games.temporalstudio.temporalengine.component;
+
+import games.temporalstudio.temporalengine.Game;
+import games.temporalstudio.temporalengine.LifeCycleContext;
+
+import java.util.function.Consumer;
+
+public class Triggerable implements Component{
+
+	private boolean triggered = false;
+	private Consumer<LifeCycleContext> action;
+
+	public Triggerable(Consumer<LifeCycleContext> action) {
+		if (action == null) {
+			Game.LOGGER.severe("Triggerable action cannot be null.");
+		}
+		this.action = action;
+	}
+
+	public void trigger(LifeCycleContext context) {
+		if (!(context instanceof GameObject object)){
+			Game.LOGGER.warning("Trigger can only be used with GameObject context.");
+			return;
+		}
+		if (!triggered){
+			Game.LOGGER.info("Triggerable triggered by GameObject: " + object.getName());
+			this.triggered = true;
+		} else {
+			Game.LOGGER.warning("Triggerable already triggered, ignoring.");
+		}
+	}
+
+	@Override
+	public void update(LifeCycleContext context, float delta) {
+		if (triggered){
+			if (!(context instanceof GameObject object)){
+				Game.LOGGER.severe("Triggerable can only be used with GameObject context.");
+				return;
+			}
+			Game.LOGGER.info("Object " + object.getName() + " is triggered, performing action.");
+			action.accept(context);
+			triggered = false;
+		}
+	}
+
+	@Override
+	public void init(LifeCycleContext context) {
+
+	}
+
+	@Override
+	public void start(LifeCycleContext context) {
+
+	}
+
+	@Override
+	public void destroy(LifeCycleContext context) {
+
+	}
+}
