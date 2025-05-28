bc1f017f2c4e84b280aeb246acb4f9f1c785a6a6
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-28 17:18:06 +0200

    Changed double to float

--- engine/src/main/java/games/temporalstudio/temporalengine/physics/PhysicsEngineLifeCycle.java
+++ engine/src/main/java/games/temporalstudio/temporalengine/physics/PhysicsEngineLifeCycle.java
@@ -4,5 +4,5 @@ import games.temporalstudio.temporalengine.LifeCycle;
 import games.temporalstudio.temporalengine.LifeCycleContext;
 
 public interface PhysicsEngineLifeCycle extends LifeCycle {
-	public abstract void compute(LifeCycleContext context, double deltaTime);
+	public abstract void compute(LifeCycleContext context, float deltaTime);
 }
