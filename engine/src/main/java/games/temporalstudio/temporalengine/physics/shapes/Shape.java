1f20b1b101f7ca91ef4415f3af2f34f6624f06ac
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-28 17:17:21 +0200

    Added cast method to get translated AABB

--- engine/src/main/java/games/temporalstudio/temporalengine/physics/shapes/Shape.java
+++ engine/src/main/java/games/temporalstudio/temporalengine/physics/shapes/Shape.java
@@ -1,8 +1,11 @@
 package games.temporalstudio.temporalengine.physics.shapes;
 
+import org.joml.Vector2f;
+
 public interface Shape {
 	public boolean intersects(Shape other);
 	public void updateShape();
 	public void setOffset(float x, float y);
 	public void setMagnitude(float x, float y);
+	public Shape cast(Vector2f translation);
 }
