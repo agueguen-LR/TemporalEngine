50a773855e69670e6526321e506a32677253b740
irisiblou <floralakatosh@gmail.com>
2025-05-31 19:23:41 +0200

    Update entity branch

--- game/src/main/java/games/temporalstudio/timecapsule/Launcher.java
+++ game/src/main/java/games/temporalstudio/timecapsule/Launcher.java
@@ -6,13 +6,14 @@ public class Launcher{
 
 	public static void main(String[] args){
 		boolean isEngineTest = false;
-
+/*
 		if(args.length > 0 && args[0].equalsIgnoreCase("engine"))
 			isEngineTest = true;
 
 		if(!isEngineTest)
 			new TimeCapsule().run(new String[]{"Time Capsule"});
-		else
+		else*/
 			new TestGame().run(new String[0]);
 	}
+
 }
