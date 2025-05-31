7cc0716f387f79c26cee5276ed7d156e7df81853
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-05-31 22:05:58 +0200

    Better Level type handling

--- game/src/main/java/games/temporalstudio/timecapsule/levels/Level.java
+++ game/src/main/java/games/temporalstudio/timecapsule/levels/Level.java
@@ -1,8 +1,4 @@
 package games.temporalstudio.timecapsule.levels;
 
-import games.temporalstudio.temporalengine.Scene;
-
 public interface Level {
-	public Scene getPastScene();
-	public Scene getFuturScene();
 }
