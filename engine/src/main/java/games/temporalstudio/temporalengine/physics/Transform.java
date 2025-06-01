64ae72ff611fa020242b00a4048be6e5ba4bc17b
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-06-01 10:28:03 +0200

    Added documentation

--- engine/src/main/java/games/temporalstudio/temporalengine/physics/Transform.java
+++ engine/src/main/java/games/temporalstudio/temporalengine/physics/Transform.java
@@ -4,36 +4,93 @@ import games.temporalstudio.temporalengine.LifeCycleContext;
 import games.temporalstudio.temporalengine.component.Component;
 import org.joml.Vector2f;
 
-public class Transform implements Component{
+/**
+ * Represents the position and scale of a game object in 2D space.
+ * This component is used to track and manipulate the spatial properties
+ * of game objects, such as their location and size.
+ *
+ * @author agueguen-LR, Xibitol
+ */
+public class Transform implements Component {
 
+	/** The default scale for all transforms (1, 1). */
 	private static final Vector2f DEFAULT_SCALE = new Vector2f(1, 1);
 
+	/** The position of the object in 2D space. */
 	private Vector2f position;
+
+	/** The scale of the object in 2D space. */
 	private Vector2f scale;
 
-	public Transform(Vector2f position, Vector2f scale){
+	/**
+	 * Constructs a Transform with the specified position and scale.
+	 *
+	 * @param position The position vector.
+	 * @param scale    The scale vector.
+	 */
+	public Transform(Vector2f position, Vector2f scale) {
 		this.position = position;
 		this.scale = scale;
 	}
-	public Transform(Vector2f position){
+
+	/**
+	 * Constructs a Transform with the specified position and default scale.
+	 *
+	 * @param position The position vector.
+	 */
+	public Transform(Vector2f position) {
 		this(position, new Vector2f(DEFAULT_SCALE));
 	}
-	public Transform(){
+
+	/**
+	 * Constructs a Transform with default position (0, 0) and default scale (1, 1).
+	 */
+	public Transform() {
 		this(new Vector2f(), new Vector2f(DEFAULT_SCALE));
 	}
 
-	// GETTERS
-	public Vector2f getPosition(){ return position; }
-	public Vector2f getScale(){ return scale; }
+	/**
+	 * Returns the position of the object.
+	 *
+	 * @return The position vector.
+	 */
+	public Vector2f getPosition() {
+		return position;
+	}
 
-	// SETTERS
-	public void setPosition(Vector2f position){
+	/**
+	 * Returns the scale of the object.
+	 *
+	 * @return The scale vector.
+	 */
+	public Vector2f getScale() {
+		return scale;
+	}
+
+	/**
+	 * Sets the position of the object.
+	 *
+	 * @param position The new position vector.
+	 */
+	public void setPosition(Vector2f position) {
 		this.position = position;
 	}
-	public void setScale(Vector2f scale){
+
+	/**
+	 * Sets the scale of the object.
+	 *
+	 * @param scale The new scale vector.
+	 */
+	public void setScale(Vector2f scale) {
 		this.scale = scale;
 	}
-	public void move(Vector2f translation){
+
+	/**
+	 * Moves the object by the specified translation vector.
+	 *
+	 * @param translation The translation vector to add to the current position.
+	 */
+	public void move(Vector2f translation) {
 		this.position.add(translation);
 	}
 
