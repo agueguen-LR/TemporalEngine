f16914ea8b734893e8b0be45b4cf3e4c8437b4a6
Xibitol <xibitol@pimous.dev>
2025-06-01 12:58:00 +0200

    Fixed NIOUtils resource reading.

--- engine/src/main/java/games/temporalstudio/temporalengine/utils/NIOUtils.java
+++ engine/src/main/java/games/temporalstudio/temporalengine/utils/NIOUtils.java
@@ -1,22 +1,31 @@
 package games.temporalstudio.temporalengine.utils;
 
-import org.lwjgl.BufferUtils;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URL;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
-import java.nio.channels.SeekableByteChannel;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.Optional;
+
+import org.lwjgl.BufferUtils;
 
 /**
  * @author Adnan FAIZE
  */
 public final class NIOUtils{
 
+	private static final int BUFFER_SIZE = 4096;
+
 	// FUNCTIONS
+    private static ByteBuffer resizeBuffer(ByteBuffer buffer, int newSize){
+        ByteBuffer newBuffer = BufferUtils.createByteBuffer(newSize);
+        buffer.flip();
+        newBuffer.put(buffer);
+        return newBuffer;
+    }
+
+	// TODO: Make this code more memory efficient.
 	/**
      * Reads a resource from the classpath or file system into a ByteBuffer.
      * @param resource The resource path.
@@ -28,25 +37,24 @@ public final class NIOUtils{
     public static ByteBuffer getResourceAsByteBuffer(String resource)
 		throws IOException
 	{
-        ByteBuffer buffer;
+		ByteBuffer buffer;
 
-		Optional<URL> opURL = Optional.ofNullable(
-			ClassLoader.getSystemResource(resource)
+        Optional<InputStream> opURL = Optional.ofNullable(
+			ClassLoader.getSystemResourceAsStream(resource)
 		);
 		if(opURL.isEmpty())
-			throw new FileNotFoundException();
+			throw new FileNotFoundException(resource);
 
-		try(SeekableByteChannel channel = Files.newByteChannel(
-			Path.of(opURL.get().getPath())
-		)){
-			buffer = BufferUtils.createByteBuffer((int) channel.size() + 1);
+		try(ReadableByteChannel rbc = Channels.newChannel(opURL.get())){
+			buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
 
-			while(channel.read(buffer) != -1);
+			while(rbc.read(buffer) != -1)
+				if(buffer.remaining() == 0)
+					buffer = resizeBuffer(buffer,
+						buffer.capacity() + BUFFER_SIZE
+					);
 		}
 
-		System.out.println(buffer);
-
-        buffer.flip();
-        return buffer;
+        return buffer.flip();
     }
 }
