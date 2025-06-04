f1c35720502c18a5b6f502fa647f16b288b08564
legoblin <inersegla@gmail.com>
2025-06-04 23:34:32 +0200

    Creation of Root

--- game/src/main/java/games/temporalstudio/timecapsule/objects/Root.java
+++ game/src/main/java/games/temporalstudio/timecapsule/objects/Root.java
@@ -1,15 +1,47 @@
 package games.temporalstudio.timecapsule.objects;
 
+import games.temporalstudio.temporalengine.Scene;
 import games.temporalstudio.temporalengine.component.GameObject;
+import games.temporalstudio.temporalengine.physics.Collider2D;
+import games.temporalstudio.temporalengine.physics.Transform;
+import games.temporalstudio.temporalengine.physics.shapes.AABB;
+import games.temporalstudio.temporalengine.rendering.component.ColorRender;
+import games.temporalstudio.temporalengine.rendering.component.Render;
+import org.joml.Vector2f;
+import org.joml.Vector4f;
 
-import javax.xml.namespace.QName;
-
-public class Root {
+class Root implements TimeObject {
     private GameObject gameObject;
 
-    public Root(String name,float oX,float oY, boolean north, boolean south, boolean west, boolean east, int length) {
-        gameObject = new GameObject(name );
+
+    //Racine nord
+    Root(String name, float oX, float oY, Scene futurScene) {
+
+        gameObject = new GameObject(name);
+        Transform transform = new Transform(new Vector2f(oX, oY));
 
 
+        Render render = new ColorRender(new Vector4f(0.8f, 0.2f, 0.2f, 1));
+        Collider2D newColider2d = new Collider2D(new AABB(transform));
+        newColider2d.setOnIntersects((objRoot, other) -> {
+            if (other instanceof GameObject otherGameObject) {
+                if (otherGameObject.getName().startsWith("Wall")) {
+
+                    //otherGameObject.removeComponent(otherGameObject.getComponent(Collider2D.class));
+                    futurScene.removeGameObject(otherGameObject);
+                }
+
+            }
+        });
+        this.gameObject.addComponent(transform);
+        this.gameObject.addComponent(newColider2d);
+        this.gameObject.addComponent(render);
+
+        futurScene.addGameObject(gameObject);
+    }
+
+    @Override
+    public GameObject getGameObject() {
+        return gameObject;
     }
 }
