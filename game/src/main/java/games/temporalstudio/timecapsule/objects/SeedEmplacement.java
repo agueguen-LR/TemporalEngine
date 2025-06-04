b06f03f427542affa87c77431e945ca527f90f9d
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-06-04 18:38:12 +0200

    Implemented SeedEmplacement

--- game/src/main/java/games/temporalstudio/timecapsule/objects/SeedEmplacement.java
+++ game/src/main/java/games/temporalstudio/timecapsule/objects/SeedEmplacement.java
@@ -1,10 +1,9 @@
 package games.temporalstudio.timecapsule.objects;
 
+import games.temporalstudio.temporalengine.Game;
 import games.temporalstudio.temporalengine.Scene;
 import games.temporalstudio.temporalengine.component.GameObject;
 import games.temporalstudio.temporalengine.component.Input;
-import games.temporalstudio.temporalengine.component.Trigger;
-import games.temporalstudio.temporalengine.component.Triggerable;
 import games.temporalstudio.temporalengine.physics.Collider2D;
 import games.temporalstudio.temporalengine.physics.Transform;
 import games.temporalstudio.temporalengine.physics.shapes.AABB;
@@ -13,33 +12,33 @@ import games.temporalstudio.temporalengine.rendering.component.Render;
 import games.temporalstudio.timecapsule.Entity.Player;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
-import org.lwjgl.glfw.GLFW;
 
 public class SeedEmplacement implements TimeObject{
     private GameObject gameObject;
-    private Scene scene;
-    private Seed seed;
 
-
-
-    public SeedEmplacement(String name, float x, float y, Player player, int keyPlayerUse) {
+    public SeedEmplacement(String name, float x, float y, Player player, int keyPlayerUse, Scene pastScene, Scene futurScene) {
         this.gameObject = new GameObject(name);
         Transform transform = new Transform(new Vector2f(x, y));
         Render render = new ColorRender(new Vector4f(0.8f, 0.2f, 0.2f, 1));
 
         Collider2D collider2D = new Collider2D(new AABB(transform));
         collider2D.setOnIntersects((context, other) -> {
-            if (other instanceof Player) {
+            if (other instanceof GameObject && other == player.getGameObject()) {
                 //Checking if the player have a seed
-                if (player.inventoryContain(seed)){
+                if (player.inventoryContains(Seed.class)){
                     if (player.getGameObject().getComponent(Input.class).isControlPressed(keyPlayerUse)){
-                        //Adding the seed of the player to the emplacement
-                        this.seed = (Seed) player.getInventoryObject(seed);
-                        //Remove the seed from player inventory
-                        player.removeFromInventory(seed);
-                        this.scene.addGameObject((this.seed.getGameObject()));
+                        gameObject.removeComponent(render);
+                        gameObject.removeComponent(collider2D);
+                        gameObject.removeComponent(transform);
 
 
+                        player.removeFromInventory(player.getInventoryObject(Seed.class));
+                        Tree pastTree = new Tree(pastScene.getName() + "_tree", x, y);
+                        pastScene.addGameObject(pastTree.getGameObject());
+                        Tree futurTree = new Tree(futurScene.getName() + "_tree", x, y);
+                        futurScene.addGameObject(futurTree.getGameObject());
+
+                        pastScene.removeGameObject(this.gameObject);
                     }
                 }
             }
