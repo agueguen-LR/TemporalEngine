95c51896d675828608624711f53bf48cfb5ce2ea
Litszro <florian.nagisa@gmail.com>
2025-06-05 00:16:04 +0200

    Modification du level 3 Afficher les tiles des objets sur le map. Création des collisions. Correction des clés, fragments et chest. Corrections des exits, wall, player, zone, following, et enemy.

--- game/src/main/java/games/temporalstudio/timecapsule/objects/Chest.java
+++ game/src/main/java/games/temporalstudio/timecapsule/objects/Chest.java
@@ -7,6 +7,7 @@ import games.temporalstudio.temporalengine.physics.Transform;
 import games.temporalstudio.temporalengine.physics.shapes.AABB;
 import games.temporalstudio.temporalengine.rendering.component.ColorRender;
 import games.temporalstudio.temporalengine.rendering.component.Render;
+import games.temporalstudio.temporalengine.rendering.component.TileRender;
 import games.temporalstudio.timecapsule.Entity.Player;
 
 import org.joml.Vector2f;
@@ -27,7 +28,7 @@ public class Chest implements TimeObject{
         Transform transform = new Transform(new Vector2f(x, y)); // Set the position of the chest
         AABB shape = new AABB(transform); // Create a component shape for the chest
         Collider2D collider = new Collider2D(shape); // Create a collider for the chest
-        Render render = new ColorRender(new Vector4f(1, 1, 1, 1)); // Create a render component for the chest
+        Render render = new TileRender("past", "chest"); // Create a render component for the chest
         collider.setRigid(true);
 
         this.chest.addComponent(transform); // Add the Transform component to the chest
