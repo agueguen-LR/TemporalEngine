3ba945dde1454ecc62838db2e8f6782159e29823
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2025-06-04 18:45:06 +0200

    implements TimeObject

--- game/src/main/java/games/temporalstudio/timecapsule/objects/Tree.java
+++ game/src/main/java/games/temporalstudio/timecapsule/objects/Tree.java
@@ -2,24 +2,27 @@ package games.temporalstudio.timecapsule.objects;
 
 import games.temporalstudio.temporalengine.component.GameObject;
 import games.temporalstudio.temporalengine.physics.Transform;
-import games.temporalstudio.temporalengine.rendering.component.ColorRender;
+import games.temporalstudio.temporalengine.rendering.Layer;
 import games.temporalstudio.temporalengine.rendering.component.Render;
+import games.temporalstudio.temporalengine.rendering.component.TileRender;
 import org.joml.Vector2f;
-import org.joml.Vector4f;
 
-public class Tree {
+public class Tree implements TimeObject{
     private GameObject gameObject;
 
     public Tree(String name, float x, float y) {
         this.gameObject = new GameObject(name);
         Transform transform = new Transform(new Vector2f(x, y));
-        Render render = new ColorRender(new Vector4f(0.8f, 0.2f, 0.2f, 1));
+        Render render = new TileRender("tree", "tree", Layer.OBJECT);
 
         this.gameObject.addComponent(transform);
         this.gameObject.addComponent(render);
 
 
+    }
 
-
+    @Override
+    public GameObject getGameObject() {
+        return gameObject;
     }
 }
