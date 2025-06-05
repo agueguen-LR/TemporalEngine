898cea210096ee840811b4f8eed978cb06f99a90
Xibitol <xibitol@pimous.dev>
2025-06-05 07:06:53 +0200

    Added home page texture to main menu.

--- game/src/main/java/games/temporalstudio/timecapsule/levels/MainMenu.java
+++ game/src/main/java/games/temporalstudio/timecapsule/levels/MainMenu.java
@@ -1,8 +1,11 @@
 package games.temporalstudio.timecapsule.levels;
 
+import org.joml.Vector2f;
+
 import games.temporalstudio.temporalengine.Scene;
 import games.temporalstudio.temporalengine.component.GameObject;
 import games.temporalstudio.temporalengine.physics.Transform;
+import games.temporalstudio.temporalengine.rendering.component.TileRender;
 import games.temporalstudio.temporalengine.rendering.component.View;
 
 public class MainMenu implements SingleLevel{
@@ -11,10 +14,19 @@ public class MainMenu implements SingleLevel{
 	public MainMenu(){
 		this.scene = new Scene("MainMenu");
 
+		GameObject bg = new GameObject("Background");
+		bg.addComponent(new Transform(
+			new Vector2f(-(640f/360 - 1)/2, 0),
+			new Vector2f(640f/360, 1)
+		));
+		bg.addComponent(new TileRender("home_page", null));
+
 		GameObject camera = new GameObject("camera");
 		camera.addComponent(new Transform());
-		camera.addComponent(new View(.1f));
+		camera.addComponent(new View(1f));
+
 		this.scene.addGameObject(camera);
+		this.scene.addGameObject(bg);
 	}
 
 	public Scene getScene(){
